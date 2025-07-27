6de7e62407f34930976814d8fefcaadf891a502a
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2025-07-27 21:39:23 +0700

    do not urllib2 cancer

--- easy_install.py
+++ easy_install.py
@@ -12,7 +12,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 import os
 import shutil
-import urllib2
+import urllib.request as urllib2
 import zipfile
 
 import ida_diskio
